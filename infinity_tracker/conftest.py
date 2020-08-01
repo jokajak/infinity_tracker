7de1dd6e92fcd89a5b3f78ba1a79cef294e83f05
jokajak <jokajak@gmail.com>
2020-08-01 17:28:41 -0400

    feature: initialization of cookiecutter django project

---
+++ infinity_tracker/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from infinity_tracker.users.models import User
+from infinity_tracker.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
