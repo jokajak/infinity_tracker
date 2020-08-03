215fe532ef6e9270e3fedb0b9d0017701d253c0c
jokajak <jokajak@gmail.com>
2020-08-03 15:40:13 -0400

    feat: support being used as a proxy

--- config/settings/local.py
+++ config/settings/local.py
@@ -11,7 +11,7 @@ SECRET_KEY = env(
     default="MYVaG61qAMZVo8r6lmO4BC74StlDFu2HFpn6jXqs1khbNUgJELrQIGiuYuOfix43",
 )
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1"]
+ALLOWED_HOSTS = ["*"]
 
 # CACHES
 # ------------------------------------------------------------------------------
