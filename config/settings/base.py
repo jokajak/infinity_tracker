cf1ed75bed8696340c98d16192d36bff84fb80e2
jokajak <jokajak@gmail.com>
2020-08-06 20:03:54 -0400

    feat: add initial migration file
    add hvac to installed apps

--- config/settings/base.py
+++ config/settings/base.py
@@ -76,6 +76,7 @@ THIRD_PARTY_APPS = [
 LOCAL_APPS = [
     "infinity_tracker.users.apps.UsersConfig",
     # Your stuff: custom apps go here
+    "hvac.apps.HvacConfig",
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
