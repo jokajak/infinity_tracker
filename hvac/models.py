015db296c26e913791e004ad8c556e6120063698
jokajak <jokajak@gmail.com>
2020-08-30 15:29:57 -0400

    bugfix(hvac): Fix syntax and add migrations
    Refs: #16

--- hvac/models.py
+++ hvac/models.py
@@ -28,8 +28,8 @@ class HVAC(models.Model):
     """HVAC Model."""
 
     serial = models.CharField(max_length=100)
-    heat_set_point = models.DecimalField(max_digits=5, decimal_places=2)
-    cool_set_point = models.DecimalField(max_digits=5, decimal_places=2)
+    heat_set_point = models.DecimalField(max_digits=5, decimal_places=2, default=0.0)
+    cool_set_point = models.DecimalField(max_digits=5, decimal_places=2, default=0.0)
     # activity = models.IntegerChoices(choices=HVACActivities.choices)
     # activity = models.Choices(
 
@@ -63,16 +63,16 @@ class Zone(models.Model):
     </zone>
     """
 
-    id = models.PositiveIntegerField()
-    name = models.CharField()
+    zone_id = models.PositiveIntegerField()
+    name = models.CharField(max_length=250)
     enabled = models.BooleanField()
-    current_activity = models.IntegerChoices(choices=ActivityChoices)
-    current_temp = models.DecimalField(max_digits=5, decimal_places=2)
+    current_activity = models.IntegerField(choices=ActivityChoices.choices)
+    current_temp = models.DecimalField(max_digits=5, decimal_places=2, default=0.0)
     current_humidity = models.PositiveIntegerField()
-    fan = models.IntegerChoices(choice=FanChoices)
-    heat_set_point = models.DecimalField(max_digits=5, decimal_places=2)
-    cool_set_point = models.DecimalField(max_digits=5, decimal_places=2)
-    hold = models.CharField()
-    otmr = models.CharField()
-    zone_conditioning = models.IntegerChoices(choices=ZoneConditioningChoices)
+    fan = models.IntegerField(choices=FanChoices.choices)
+    heat_set_point = models.DecimalField(max_digits=5, decimal_places=2, default=0.0)
+    cool_set_point = models.DecimalField(max_digits=5, decimal_places=2, default=0.0)
+    hold = models.CharField(max_length=100)
+    otmr = models.CharField(max_length=100)
+    zone_conditioning = models.IntegerField(choices=ZoneConditioningChoices.choices)
     damper_position = models.PositiveIntegerField()
